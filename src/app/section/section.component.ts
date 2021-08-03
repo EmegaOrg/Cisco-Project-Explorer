08fc83a994c6cda813b5c2ae5e3ef2cb0d737a1c
Kushal Gupta <kushalgupta0106@gmail.com>
2021-08-03 10:35:14 +0530

    home page css change

--- src/app/section/section.component.ts
+++ src/app/section/section.component.ts
@@ -8,7 +8,7 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
   providers: [NgbCarouselConfig] 
 })
 export class SectionComponent implements OnInit {
-  images = [700, 800, 807].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  images = ["https://cdn.thewirecutter.com/wp-content/uploads/2020/04/laptops-lowres-2x1--1024x512.jpg", "https://img.electronicdesign.com/files/base/ebm/electronicdesign/image/2021/02/GettyImages_155602278.601b1e4618a25.png?auto=format&fit=crop&h=556&w=1000&q=60", "https://st2.depositphotos.com/1518767/11513/i/600/depositphotos_115132928-stock-photo-shiny-wifi-icon-on-black.jpg"];
   constructor(config: NgbCarouselConfig) { 
     config.interval = 2000;
     config.keyboard = true;
